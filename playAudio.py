0fc0458caf7e15dde900f28a6cc1d34b1d6e0c9a
mingyaoxiao <mx63@scarletmail.rutgers.edu>
2019-12-15 17:47:24 -0500

    Added cmd line param functionality

--- playAudio.py
+++ playAudio.py
@@ -1,9 +1,16 @@
-
-
+import os
+import sys, getopt
 import pyglet
 import arcade
 
-music = pyglet.resource.media('songs/relaxdaily_ocean_breeze.m4a')
+outputFile = ''
+opts, args = getopt.getopt(sys.argv[1:],"o:")
+for opt, arg in opts:
+    if opt in ("-o", "--ofile"):
+        outputFile = arg
+        print(outputFile)
+
+music = pyglet.resource.media(outputFile)
 music.play()
 
 pyglet.app.run()

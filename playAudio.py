f0f786833f369728e5c2873017e5d24357dc324d
mingyaoxiao <mx63@scarletmail.rutgers.edu>
2019-12-16 16:27:12 -0500

    Can play folders(playlists)

--- playAudio.py
+++ playAudio.py
@@ -4,13 +4,24 @@ import pyglet
 import arcade
 
 outputFile = ''
-opts, args = getopt.getopt(sys.argv[1:],"o:")
+outputDir = ''
+opts, args = getopt.getopt(sys.argv[1:],"p:")
 for opt, arg in opts:
+    print(opt)
     if opt in ("-o", "--ofile"):
         outputFile = arg
         print(outputFile)
+    if opt in ("-p"):
+        outputDir = arg
+        print(outputDir)
 
-music = pyglet.resource.media(outputFile)
-music.play()
+if outputDir == '':
+    music = pyglet.resource.media(outputFile)
+    music.play()
+else:
+   for file in os.listdir(outputDir):
+        print(file)
+        music = pyglet.resource.media("songs/"+outputDir+"/"+file)
+        music.play()
+        pyglet.app.run()
 
-pyglet.app.run()
